aac5e4bd7857be3ed8a4e67a95b17a15638e5fde
vmcguire6377 <79176079+vmcguire6377@users.noreply.github.com>
2021-04-25 18:38:25 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@
      }
      var apiKey = "f38785180bae51b68a019a1c52c15dd3";
 
-     fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=f38785180bae51b68a019a1c52c15dd3")
+     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=f38785180bae51b68a019a1c52c15dd3")
          .then(function (resp) { return resp.json() })
          .then(function (data) {
              console.log(data);
@@ -35,7 +35,7 @@
  };
 
      function weatherForecast(cityName) {
-     fetch("http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=f38785180bae51b68a019a1c52c15dd3")
+     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial" + "&appid=f38785180bae51b68a019a1c52c15dd3")
          .then(function (resp) { return resp.json() }) // Convert to json
          .then(function (data) {
              console.log(data);
@@ -89,49 +89,6 @@ function showDay2(data) {
      return desiredTime.toLocaleString();
  };
 
-
-
-
-//add function to respond to button click, When the search button is clicked,
-//the embedded api should search info for the input city.
-/*</h2 addEventListener("click", () => {});
-//let long;
-//let lat;
-//accessing location of user
-if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-        //storing longitude and latitude in variables
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
-        var base = "https://api.openweathermap.org/data/2.5/weather?q=${city name}&${apiKey}";
-        console.log(base);
-        //use fetch to retrieve data
-        fetch(base).then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            var {temp} = data.main;
-            var {place} = data.name;
-            var {description, icon } = data.weather[0];
-            var {sunrise, sunset} = data.sys;
-
-            var iconUrl = "http://openweathermap.org/img/wn/${icon}@2x.png";
-            var fahrenheit = (temp * 9) / 5 + 32;
-
-            //converting epoch time to gmt
-            var sunriseGMT = new Date(sunrise * 1000);
-            var sunsetGMT = new Date(sunset * 1000);
-            //interacting with the DOM to present data
-            loc.textContent = "${place}";
-desc.textContent = "${description}";
-tempC.textContent = "${temp.toFixed(2)} C";
-tempF.textContent = "${temp.toFixed(2)} F";
-sunriseDOM.textContent = "${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}";
-sunsetDOM.textContent = "${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}";
-        });
-    });
-
-}
     
 
 
@@ -144,7 +101,4 @@ sunsetDOM.textContent = "${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocale
 
 
 
-//when the info is captured for the searched city, the current weather data should display
-//in the card body; and the city searched should be saved in local storage and display
-//in a placeholder form under the search area.
-*/
+
